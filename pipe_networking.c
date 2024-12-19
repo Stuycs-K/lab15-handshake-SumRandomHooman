b026ef0e819a1626e9a297ac967cb6d066fc5371
Jason Liu <jliu50@stuy.edu>
2024-12-19 14:50:41 -0500

    end of class commit

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,4 +1,16 @@
 #include "pipe_networking.h"
+
+int rand(){ // generates random int
+  int buff[1];
+  int random = open("/dev/urandom", O_RDONLY, 0);
+  read(random,buff,4);
+  close(random);
+  if(buff[0] < 0){
+    buff[0] *= -1;
+  }
+  return *buff;
+}
+
 //UPSTREAM = to the server / from the client
 //DOWNSTREAM = to the client / from the server
 /*=========================
@@ -10,12 +22,20 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-  int from_client = 0;
-  return from_client;
+  // Create WKP
+  char * wkp = "./WKP";
+  mkfifo(wkp, 0666);
+  printf("WKP created\n");
+  // Open WKP (Read)
+  int syn = open(WKP, O_RDONLY);
+  // Remove WKP
+  remove(wkp);
+  printf("WKP removed\n");
+  return syn;
 }
 
 /*=========================
-  server_handshake 
+  server_handshake
   args: int * to_client
 
   Performs the server side pipe 3 way handshake.
@@ -24,6 +44,25 @@ int server_setup() {
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
 int server_handshake(int *to_client) {
+  // Server Setup
+  int syn = server_setup();
+  // Read PP
+  char pp[256];
+  read(syn, pp, sizeof(pp));
+  printf("Read pp named %s\n", pp);
+  // Opening PP (Write)
+  int syn_ack = open(pp, O_WRONLY);
+  // Making random SYN_ACK
+  int random = rand();
+  char buffer[256];
+  sprintf(buffer, "%d", random);
+  // Sending SYN_ACK
+  write(syn_ack, buffer, strlen(buffer)+1);
+  
+  // Reading ACK
+  char recieved[256];
+  read(syn, recieved, sizeof(received));
+  printf("Read wkp for mesage %s\n", recieved);
   int from_client;
   return from_client;
 }
@@ -39,6 +78,29 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  // WKP Address
+  char * wkp = "./WKP";
+  // Create PP
+  char * pp = "./PP";
+  mkfifo(pp, 0666);
+  // Open WKP (Write)
+  int syn = open(wkp, O_WRONLY);
+  // Sending PP
+  write(syn, pp, strlen(pp)+1);
+  // Open PP (Read)
+  int syn_ack = open(pp, O_RDONLY);
+  // Remove PP
+  remove(pp);
+  // Reading SYN_ACK
+  char buffer[256];
+  read(syn_ack, buffer, sizeof(buffer));
+  // Creating ACK
+  int received = atoi(buffer);
+  recieved ++;
+  char sending[256];
+  sprintf(sending, "%d", buffer);
+  // Sending ACK
+  write(syn, sending, strlen(sending)+1);
   int from_server;
   return from_server;
 }
@@ -56,5 +118,3 @@ int server_connect(int from_client) {
   int to_client  = 0;
   return to_client;
 }
-
-
