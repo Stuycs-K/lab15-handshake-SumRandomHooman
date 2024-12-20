2aedc458c323f8cfe7b1c90e52d282b305dfc678
Jason Liu <jliu50@stuy.edu>
2024-12-20 14:23:54 -0500

    tested addresses

--- basic_client.c
+++ basic_client.c
@@ -6,4 +6,10 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+  // 
+  // printf("Handshake achieved\n");
+  // write(to_server, "a", 1);
+  // char buffer[2];
+  // read(from_server, buffer, sizeof(buffer));
+  // printf("Send and then recieved %s\n", buffer);
 }
--- basic_server.c
+++ basic_server.c
@@ -5,4 +5,10 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+  // 
+  // printf("Handshake achieved\n");
+  // char buffer[2];
+  // read(from_client, buffer, sizeof(buffer));
+  // printf("Recieved %s and sent a message\n", buffer);
+  // write(to_client, "b", 1);
 }
