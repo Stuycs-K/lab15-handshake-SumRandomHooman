cb9fd45cdee2f448ec6102a9208da777787bbd72
Jason Liu <sumrandomhooman@gmail.com>
2025-01-02 20:23:31 -0500

    minor formatting

--- basic_client.c
+++ basic_client.c
@@ -5,9 +5,9 @@ int main() {
   int to_server;
   int from_server;
 
-  printf("Attempting Hand Shake\n");
+  // printf("Attempting Hand Shake\n");
   from_server = client_handshake( &to_server );
-  printf("Hand Shake Success\n");
+  // printf("Hand Shake Success\n");
   
   int recievedvalue;
   while(read(from_server, &recievedvalue, sizeof(int))){
